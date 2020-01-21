8307cec7f820b39ba236993e5495db4a0511d6f5
olovm <olov.mckie@ub.uu.se>
2020-01-21 14:55:16 +0000

    Adding factories to test

--- src/test/java/se/uu/ub/cora/metacreator/collection/CollectionVarFromItemCollectionCreatorTest.java
+++ src/test/java/se/uu/ub/cora/metacreator/collection/CollectionVarFromItemCollectionCreatorTest.java
@@ -10,6 +10,9 @@ import se.uu.ub.cora.data.DataAtomicProvider;
 import se.uu.ub.cora.data.DataGroup;
 import se.uu.ub.cora.data.DataGroupFactory;
 import se.uu.ub.cora.data.DataGroupProvider;
+import se.uu.ub.cora.data.DataRecordLinkFactory;
+import se.uu.ub.cora.data.DataRecordLinkProvider;
+import se.uu.ub.cora.metacreator.DataRecordLinkFactorySpy;
 import se.uu.ub.cora.metacreator.dependency.SpiderInstanceFactorySpy;
 import se.uu.ub.cora.metacreator.dependency.SpiderRecordCreatorSpy;
 import se.uu.ub.cora.metacreator.recordtype.DataAtomicFactorySpy;
@@ -22,6 +25,7 @@ public class CollectionVarFromItemCollectionCreatorTest {
 	private String authToken;
 	private DataGroupFactory dataGroupFactory;
 	private DataAtomicFactory dataAtomicFactory;
+	private DataRecordLinkFactory dataRecordLinkFactory;
 
 	@BeforeMethod
 	public void setUp() {
@@ -31,6 +35,8 @@ public class CollectionVarFromItemCollectionCreatorTest {
 		DataAtomicProvider.setDataAtomicFactory(dataAtomicFactory);
 		instanceFactory = new SpiderInstanceFactorySpy();
 		SpiderInstanceProvider.setSpiderInstanceFactory(instanceFactory);
+		dataRecordLinkFactory = new DataRecordLinkFactorySpy();
+		DataRecordLinkProvider.setDataRecordLinkFactory(dataRecordLinkFactory);
 		authToken = "testUser";
 	}
 
