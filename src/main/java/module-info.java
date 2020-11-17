33e37ac624e01d353a089cabcc65f4ed794af5d4
olovm <olov.mckie@ub.uu.se>
2020-11-17 10:17:35 +0000

    removing unused code

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,8 +8,6 @@ module se.uu.ub.cora.metacreator {
 	requires transitive se.uu.ub.cora.spider;
 	requires se.uu.ub.cora.logger;
 
-	exports se.uu.ub.cora.metacreator.extended;
-
 	provides se.uu.ub.cora.spider.extendedfunctionality.ExtendedFunctionalityFactory
 			with CollectionMetaCreatorFactory, MetadataExtendedFunctionalityFactory,
 			PermissionExtendedFunctionalityFactory, RecordTypeExtendedFunctionalityFactory,
