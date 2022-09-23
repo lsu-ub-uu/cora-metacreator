53a964562aee137362cc6a7dc5af40e0b0c0bb5f
olovm <olov.mckie@ub.uu.se>
2022-09-23 13:41:30 +0000

    updates for changes in storage and recordHandler

--- src/test/java/se/uu/ub/cora/metacreator/dependency/RecordStorageSpy.java
+++ src/test/java/se/uu/ub/cora/metacreator/dependency/RecordStorageSpy.java
@@ -45,12 +45,6 @@ public class RecordStorageSpy implements RecordStorage {
 	}
 
 	@Override
-	public StorageReadResult readAbstractList(String type, DataGroup filter) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public Collection<DataGroup> generateLinkCollectionPointingToRecord(String type, String id) {
 		// TODO Auto-generated method stub
 		return null;
@@ -65,13 +59,6 @@ public class RecordStorageSpy implements RecordStorage {
 
 	@Override
 	public long getTotalNumberOfRecordsForType(List<String> type, DataGroup filter) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public long getTotalNumberOfRecordsForAbstractType(String abstractType,
-			List<String> implementingTypes, DataGroup filter) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
