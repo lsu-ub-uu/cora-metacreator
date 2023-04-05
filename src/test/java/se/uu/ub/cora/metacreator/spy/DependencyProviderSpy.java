6d4f3a754eafffcf9c4e7cab9af73fe9529e6c4e
olovm <olov.mckie@ub.uu.se>
2023-04-05 14:50:40 +0000

    more work

--- src/test/java/se/uu/ub/cora/metacreator/spy/DependencyProviderSpy.java
+++ src/test/java/se/uu/ub/cora/metacreator/spy/DependencyProviderSpy.java
@@ -5,6 +5,7 @@ import se.uu.ub.cora.bookkeeper.recordpart.DataRedactor;
 import se.uu.ub.cora.bookkeeper.recordtype.RecordTypeHandler;
 import se.uu.ub.cora.bookkeeper.termcollector.DataGroupTermCollector;
 import se.uu.ub.cora.bookkeeper.validator.DataValidator;
+import se.uu.ub.cora.data.DataRecordGroup;
 import se.uu.ub.cora.search.RecordIndexer;
 import se.uu.ub.cora.search.RecordSearch;
 import se.uu.ub.cora.spider.authentication.Authenticator;
@@ -133,6 +134,13 @@ public class DependencyProviderSpy implements SpiderDependencyProvider {
 
 	@Override
 	public DataGroupToFilter getDataGroupToFilterConverter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RecordTypeHandler getRecordTypeHandlerUsingDataRecordGroup(
+			DataRecordGroup dataRecordGroup) {
 		// TODO Auto-generated method stub
 		return null;
 	}
